d0c5d00882b04d4b24663d63878620aba6861cb0
Raul <ratiugabrielraul@gmail.com>
2019-10-16 12:46:55 +0300

    added cart controller and handled cors

---
+++ src/main/java/org/fasttrackit/onlinecommerceshop/web/CartController.java
@@ -0,0 +1,37 @@
+package org.fasttrackit.onlinecommerceshop.web;
+
+import org.fasttrackit.onlinecommerceshop.service.CartService;
+import org.fasttrackit.onlinecommerceshop.transfer.cart.AddProductToCartRequest;
+import org.fasttrackit.onlinecommerceshop.transfer.cart.CartResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+
+private final CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @PutMapping
+    public ResponseEntity addProductToCart(@RequestBody @Valid AddProductToCartRequest request) {
+        cartService.addProductToCart(request);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CartResponse> getCart(@PathVariable("id") long customerId) {
+        CartResponse cart = cartService.getCart(customerId);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+
+
+    }
+}
